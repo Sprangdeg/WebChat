1da376868e81acafc2641c3d6920d4589225c7f3
bonstrom <d.bonstrom@gmail.com>
2017-05-07 22:44:56 +0200

    redux-router and small changes to text

--- src/LoginComponent.js
+++ src/LoginComponent.js
@@ -6,11 +6,11 @@ export default class Login extends React.Component {
   render() {
     return (
       <Modal isOpen={true}>
-				<ModalHeader text="Modal Header" />
+				<ModalHeader text="Login in to chat" />
 				<ModalBody>
 					<Form>
-						<FormField label="Email address" htmlFor="basic-form-input-email">
-							<FormInput autoFocus type="email" placeholder="Enter email" name="basic-form-input-email" />
+						<FormField label="Username" htmlFor="basic-form-input-email">
+							<FormInput autoFocus type="text" placeholder="Enter username" name="basic-form-input-email" />
 						</FormField>
 						<FormField label="Password" htmlFor="basic-form-input-password">
 							<FormInput type="password" placeholder="Password" name="basic-form-input-password" />
@@ -18,8 +18,8 @@ export default class Login extends React.Component {
 					</Form>
 				</ModalBody>
 				<ModalFooter>
-					<Button type="primary">Login</Button>
-					<Button type="link-cancel">Cancel</Button>
+					<Button type="primary" style={{margin: 3}} onClick={loginUser}>Login</Button>
+					<Button type="default-danger" style={{margin: 3}} onClick={alert("What should happen?")}>Cancel</Button>
 				</ModalFooter>
 			</Modal>
     );
